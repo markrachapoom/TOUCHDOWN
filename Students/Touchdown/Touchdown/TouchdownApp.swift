9a68723dd4e64347540b3b56a86266191059d1b8
markkypanc <rphanchotong@gmail.com>
2021-03-28 17:53:18 +0700

    Create new project

---
+++ Students/Touchdown/Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Mark Rachapoom on 28/3/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
