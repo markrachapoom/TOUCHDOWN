b4e1960d42a1efcf4a15234f811f59a84e7c170a
markkypanc <rphanchotong@gmail.com>
2021-04-02 00:30:38 +0700

    Add brands layout

--- Students/Touchdown/Touchdown/Utility/Constant.swift
+++ Students/Touchdown/Touchdown/Utility/Constant.swift
@@ -8,19 +8,21 @@
 import Foundation
 import SwiftUI
 
-// DATA
+// MARK: - DATA
 
 let players: [Player] = Bundle.main.decode("player.json")
 let categories: [Category] = Bundle.main.decode("category.json")
 let products: [Product] = Bundle.main.decode("product.json")
+let brands: [Brand] = Bundle.main.decode("brand.json")
+
 let sampleProduct: Product = products[0]
 
-// COLOR
+// MARK: - COLOR
 
 let colorBackground: Color = Color("ColorBackground")
 let colorGray: Color = Color(UIColor.systemGray4)
 
-// LAYOUT
+// MARK: - LAYOUT
 
 let columnSpacing: CGFloat = 10
 let rowSpacing: CGFloat = 10
@@ -28,12 +30,16 @@ var gridLayout: [GridItem] {
   return Array(repeating: GridItem(.flexible(), spacing: rowSpacing), count: 2)
 }
 
-// UX
+// MARK: - UX
 
 let feedback = UIImpactFeedbackGenerator(style: .medium)
 
-// API
-// IMAGE
-// FONT
-// STRING
-// MISC
+// MARK: - API
+
+// MARK: - IMAGE
+
+// MARK: - FONT
+
+// MARK: - STRING
+
+// MARK: - MISC
